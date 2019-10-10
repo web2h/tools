aac17a762b4f6573c34e2c595c30fdc439bb79f1
ads1x704 <ads1x704@host.docker.internal>
2019-10-10 14:19:53 -0400

    Added some methods

--- src/main/java/com/web2h/tools/date/DateTools.java
+++ src/main/java/com/web2h/tools/date/DateTools.java
@@ -1,5 +1,8 @@
 package com.web2h.tools.date;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Date;
 
 import org.apache.commons.lang3.Validate;
@@ -18,5 +21,23 @@ public class DateTools {
 		Validate.notNull(date1, "Date 1 should not be null when calling DateTools.isBefore");
 		Validate.notNull(date2, "Date 2 should not be null when calling DateTools.isBefore");
 		return date1.getTime() < date2.getTime();
+	}
+
+	public static boolean isPast(Date date) {
+		return isBefore(date, new Date());
+	}
+
+	public static Date getDateMinusHours(int hours) {
+		LocalDateTime localDateTime = LocalDateTime.now();
+		localDateTime = localDateTime.minusHours(hours);
+		ZonedDateTime zonedDateTime = localDateTime.atZone(ZoneId.systemDefault());
+		return Date.from(zonedDateTime.toInstant());
+	}
+
+	public static Date getDatePlusHours(int hours) {
+		LocalDateTime localDateTime = LocalDateTime.now();
+		localDateTime = localDateTime.plusHours(hours);
+		ZonedDateTime zonedDateTime = localDateTime.atZone(ZoneId.systemDefault());
+		return Date.from(zonedDateTime.toInstant());
 	}
 }
